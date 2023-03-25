b020d5bdb2f5148534362d8a6be3d80e73fc7a09
ubermanu <manu@paca.happy-dev.fr>
2023-03-25 16:45:51 +0100

    setup design

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,33 @@
+// tailwind.config.js
+const colors = require('tailwindcss/colors')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'media',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    colors: {
+      gray: colors.zinc,
+      blue: colors.sky,
+      red: colors.rose,
+      pink: colors.fuchsia,
+      white: colors.white,
+    },
+    extend: {
+      spacing: {
+        '128': '32rem',
+        '144': '36rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
   plugins: [],
 }
