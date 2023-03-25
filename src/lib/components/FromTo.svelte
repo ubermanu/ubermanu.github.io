fc291cfde0916ee7cb9602e950eedc405265c404
ubermanu <manu@paca.happy-dev.fr>
2023-03-25 16:57:07 +0100

    update from to

--- src/lib/components/FromTo.svelte
+++ src/lib/components/FromTo.svelte
@@ -15,13 +15,48 @@
   export let format = 'long'
 
   function formatDate(date, locale = 'fr-FR') {
-    const options = { year: 'numeric', month: 'numeric', day: 'numeric' }
+    const options = { year: 'numeric' }
+    const [year, month, day] = date.split('-')
+
+    if (month) {
+      options.month = 'numeric'
+    }
+
+    if (day) {
+      options.day = 'numeric'
+    }
+
+    if (month && !day) {
+      options.month = 'long'
+    }
+
     return new Date(date).toLocaleDateString(locale, options)
+  }
+
+  function getStartDatePrefix(date) {
+    const [, month, day] = date.split('-')
+    return month && !day ? 'De' : 'Du'
+  }
+
+  function getEndDatePrefix(date) {
+    const [, month, day] = date.split('-')
+    return month && !day ? 'à' : 'au'
+  }
+
+  function hasDay(date) {
+    const [, , day] = date.split('-')
+    return !!day
   }
 </script>
 
-{#if endDate}
-  Du {formatDate(startDate)} au {formatDate(endDate)}
+{#if startDate === endDate}
+  {formatDate(startDate)}
+{:else if endDate}
+  {getStartDatePrefix(startDate)}
+  {formatDate(startDate)}
+  {getEndDatePrefix(startDate)}
+  {formatDate(endDate)}
 {:else}
-  Depuis le {formatDate(startDate)}
+  Depuis {#if hasDay(startDate)}le{/if}
+  {formatDate(startDate)}
 {/if}
